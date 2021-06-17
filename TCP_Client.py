5e96bca7350275d0ca0a04463bcec2582c27f082
greylupi <74424178+greylupi@users.noreply.github.com>
2021-06-17 18:32:19 -0400

    Create TCP_Client.py

---
+++ TCP_Client.py
@@ -0,0 +1,17 @@
+import socket
+
+host = "www.google.com"
+port = 80
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect((host,port))
+
+client.send(b"GET / HTTP/1.1\r\nHost:www.google.com\r\n\r\n")
+
+response = client.recv(4096)
+
+print(response.decode())
+
+client.close()
+
