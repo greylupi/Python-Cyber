e8fbe362fa681793df0a654cbe9430386298c447
greylupi <74424178+greylupi@users.noreply.github.com>
2021-06-21 09:01:20 -0400

    Create TCP_Client_No_Args.py
    TCP Client without Args

---
+++ TCP_Client_No_Args.py
@@ -0,0 +1,16 @@
+import socket
+
+host = www.google.com
+port = 80
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect((host,port))
+
+client.send("GET / HTTP/1.1\r\n\r\n")
+
+response = client.recv(1024)
+
+print(response.decode())
+
+client.close()
