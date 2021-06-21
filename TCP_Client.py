1c8225a77de599c5614c01bcee1ecaf9c6fa7305
greylupi <74424178+greylupi@users.noreply.github.com>
2021-06-21 10:48:18 -0400

    Updates
    Made changes

--- TCP_Client.py
+++ TCP_Client.py
@@ -8,7 +8,7 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((host,port))
 
-client.send("GET / HTTP/1.1\r\n\r\n")
+client.send(b"GET / HTTP/1.1\r\n\r\n")
 
 response = client.recv(1024)
 
--- TCP_Client_No_Args.py
+++ TCP_Client_No_Args.py
@@ -1,13 +1,13 @@
 import socket
 
-host = www.google.com
+host = "www.google.com"
 port = 80
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((host,port))
 
-client.send("GET / HTTP/1.1\r\n\r\n")
+client.send(b"GET / HTTP/1.1\r\n\r\n")
 
 response = client.recv(1024)
 
