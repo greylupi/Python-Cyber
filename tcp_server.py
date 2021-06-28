7183435883777e915f2869539450dee93b97850a
greylupi <74424178+greylupi@users.noreply.github.com>
2021-06-28 15:23:25 -0400

    Update tcp_server.py
    Made changes

--- tcp_server.py
+++ tcp_server.py
@@ -1,11 +1,15 @@
 import socket
 
-IP - "0.0.0.0"
+IP = "127.0.0.1"
 PORT = 1337
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((IP,PORT))
 server.listen()
-server.recv(1024)
-server.send(b'Message received')
-server.close()
+print('[*] Listening for connections...')
+client, (ip,port) = server.accept()
+
+client.send(b'You have successfully connected to the TCP server')
+
+data = client.recv(1024)
+print(f'[*] Received: {data.decode("utf-8")}')
